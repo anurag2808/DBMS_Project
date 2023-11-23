43edb0f38e58751dd14534a9a829dde6c83c8ea5
WINGMAN100 <siddhant.guha03@gmail.com>
2023-11-24 00:52:26 +0530

    modified js

--- index.js
+++ index.js
@@ -1,35 +1,18 @@
-const http = require('http');
-const fs=require('fs');
-const { start } = require('repl');
-const hostname = '127.0.0.1';
-const port = 3000;
-const dbms=fs.readFileSync('./dbms.html');
-const loginstudent=fs.readFileSync('./loginstudent.html');
-const loginstaff=fs.readFileSync('./loginstaff.html');
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  let url=req.url;
-  link.rel='stylesheet';
-  res.setHeader('Content-Type', 'text/html');
-  link.href='style.css';
-  if(url =='/')
-  {
-      res.end(dbms);
-  }
-  else if(url =='/loginstudent')
-  {
-      res.end(loginstudent);
-  }
- else if(url =='/loginstaff')
-  {
-      res.end(loginstaff);
-  }
-  else{
-    res.end("<h1>page not found</h1>");
-    res.statusCode=404;
-    
-  }
+const express= require('express');
+const path=require('path');
+const app= express();
+const port=3000;
+app.use( express.static('public'));
+app.get("/",(req,res)=>{
+    res.sendFile(path.join(__dirname,'dbms.html'));
 });
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+app.get("/loginstaff",(req,res)=>{
+    res.sendFile(path.join(__dirname,'loginstaff.html'));
+});
+app.get("/loginstudent",(req,res)=>{
+    res.sendFile(path.join(__dirname,'loginstudent.html'));
+});
+//START THE SERVER
+app.listen(port,()=>{
+    console.log(`server started succesfully at http:\\localhost:${port}`);
 });
