0b9072710dc565a78f518d4fdbc029fe52edb2d7
anurag2808 <anuragbanerjee2004@gmail.com>
2023-11-25 23:56:18 +0530

    Populated main 5 tables

--- ERP.sql
+++ ERP.sql
@@ -10,34 +10,30 @@ CREATE TABLE teacher (
     course_id INT,
     FOREIGN KEY (course_id) REFERENCES course(course_id)
 );
-
 CREATE TABLE panel (
     panel_name VARCHAR(30) PRIMARY KEY,
     total_student INT
 );
-
 CREATE TABLE Subjects (
     panel_name VARCHAR(20),
     course_id INT ,
     FOREIGN KEY (panel_name) REFERENCES panel(panel_name),
     FOREIGN KEY (course_id) REFERENCES course(course_id)
 );
-
 CREATE TABLE student (
     sprn INT PRIMARY KEY,
     fname VARCHAR(20),
     mname VARCHAR(20),
     lname VARCHAR(20),
     roll_no VARCHAR(20),
-    passwords VARCHAR(15)
+    passwords VARCHAR(15),
+    panel_name VARCHAR(30)
 );
-
 CREATE TABLE course (
     course_id INT PRIMARY KEY,
     course_name VARCHAR(15),
     tprn INT
 );
-
 CREATE TABLE attendance (
     sprn INT,
     tprn INT,
@@ -47,7 +43,6 @@ CREATE TABLE attendance (
     FOREIGN KEY (sprn) REFERENCES student(sprn),
     FOREIGN KEY (tprn) REFERENCES teacher(tprn)
 );
-
 CREATE TABLE timetable (
     tprn INT,
     day VARCHAR(20),
@@ -57,11 +52,18 @@ CREATE TABLE timetable (
     FOREIGN KEY (tprn) REFERENCES teacher(tprn),
     FOREIGN KEY (panel_name) REFERENCES panel(panel_name)
 );
-insert into course values('104','MMC','10412023');
-insert into teacher values('10412023','shamla','mantri','test@12345','104');
-insert into student values('1032220989','rishuraj','-','singh','PD28','test@12345');
+
+
+
 select * from course;
 select * from teacher;	
+select * from student;
+select * from Subjects;
+select * from panel;
+
+
+
+
 DELIMITER //
 CREATE FUNCTION validate_teacher_login(tprn_param VARCHAR(255), password_param VARCHAR(255)) RETURNS INT
 BEGIN
@@ -91,9 +93,94 @@ BEGIN
     END IF;
 END//
 DELIMITER ;
-SELECT validate_teacher_login('10412023', 'test@12345') AS teacher_login_result;
-SELECT validate_student_login('1032220989', 'test@12345') AS student_login_result;
 
-DROP FUNCTION IF EXISTS validate_student_login;
+
+
+
+insert into student values('103000','Rajesh','Rahul','Pandey','28','test@12345','A');
+insert into student values('103001','Ramesh','prajval','Prajapati','06','test@12345','A');
+insert into student values('103002','Pravin','Babu','Patidar','17','test@12345','A');
+insert into student values('103003','Jayesh','Jaybhai','Patel','04','test@12345','A');
+insert into student values('103004','Sujal','Prathmesh','Deshmukh','23','test@12345','A');
+insert into student values('103005','Pratham','Ravindra','Rathod','32','test@12345','A');
+insert into student values('103006','Johan','Rahul','Pandey','28','test@12345','A');
+insert into student values('103007','Rishab','Manish','Burad','36','test@12345','A');
+insert into student values('103008','Shubham','Datta','Rai','42','test@12345','A');
+insert into student values('103009','Samarth','Pankaj','Hatwar','28','test@12345','A');
+INSERT INTO student (sprn, fname, mname, lname, roll_no, passwords, panel_name)
+VALUES('103010', 'John', 'Doe', 'Smith', '01', 'test@12345', 'B'),
+('103011', 'Jane', 'Marie', 'Johnson', '02', 'test@12345', 'B'),
+('103012', 'Robert', 'William', 'Miller', '03', 'test@12345', 'B'),
+('103013', 'Emily', 'Grace', 'Davis', '04', 'test@12345', 'B'),
+('103014', 'Michael', 'Alexander', 'Brown', '05', 'test@12345', 'B'),
+('103015', 'Sophia', 'Isabella', 'Taylor', '06', 'test@12345', 'B'),
+('103016', 'Daniel', 'Joseph', 'Anderson', '07', 'test@12345', 'B'),
+('103017', 'Olivia', 'Evelyn', 'Clark', '08', 'test@12345', 'B'),
+('103018', 'Samarth', 'Pankaj', 'Hatwar', '09', 'test@12345', 'B'),
+('103019', 'Ava', 'Madison', 'White', '10', 'test@12345', 'B');
+INSERT INTO student (sprn, fname, mname, lname, roll_no, passwords, panel_name)
+VALUES('103020', 'Alice', 'Marie', 'Johnson', '01', 'test@12345', 'C'),
+('103021', 'Bob', 'William', 'Miller', '02', 'test@12345', 'C'),
+('103022', 'Charlie', 'Doe', 'Smith', '03', 'test@12345', 'C'),
+('103023', 'David', 'Grace', 'Davis', '04', 'test@12345', 'C'),
+('103024', 'Eva', 'Alexander', 'Brown', '05', 'test@12345', 'C'),
+('103025', 'Frank', 'Isabella', 'Taylor', '06', 'test@12345', 'C'),
+('103026', 'Grace', 'Joseph', 'Anderson', '07', 'test@12345', 'C'),
+('103027', 'Henry', 'Evelyn', 'Clark', '08', 'test@12345', 'C'),
+('103028', 'Ivy', 'Pankaj', 'Hatwar', '09', 'test@12345', 'C'),
+('103029', 'Jack', 'Madison', 'White', '10', 'test@12345', 'C');
+INSERT INTO student (sprn, fname, mname, lname, roll_no, passwords, panel_name)
+VALUES('103030', 'Emma', 'Rose', 'Anderson', '01', 'test@12345', 'D'),
+('103031', 'Finn', 'Michael', 'Thompson', '02', 'test@12345', 'D'),
+('103032', 'Giselle', 'Daniel', 'Harrison', '03', 'test@12345', 'D'),
+('103033', 'Harry', 'Grace', 'Carter', '04', 'test@12345', 'D'),
+('103034', 'Isla', 'William', 'Bennett', '05', 'test@12345', 'D'),
+('103035', 'Jacob', 'Isabella', 'Fisher', '06', 'test@12345', 'D'),
+('103036', 'Katie', 'Joseph', 'Gardner', '07', 'test@12345', 'D'),
+('103037', 'Leo', 'Evelyn', 'Parker', '08', 'test@12345', 'D'),
+('103038', 'Mia', 'Ethan', 'Wright', '09', 'test@12345', 'D'),
+('103039', 'Noah', 'Madison', 'Hayes', '10', 'test@12345', 'D');
+
+
+insert into course values('2001','DBMS',102000);
+insert into course values('2002','OS',102001);
+insert into course values('2003','FDS',102002);
+insert into course values('2004','MMC',102003);
+insert into course values('2005','DETT',102004);
+
+
+insert into teacher values ('102000','Shilpa','Sonavane','test@12345','2001');
+insert into teacher values ('102001','Ranjana','Agrawal','test@12345','2002');
+insert into teacher values ('102002','Shridevi','Karande','test@12345','2003');
+insert into teacher values ('102003','Shamla','Mantri','test@12345','2004');
+insert into teacher values ('102004','Prerna','Jain','test@12345','2005');
+
+
+insert into panel values('A','10');
+insert into panel values('B','10');
+insert into panel values('C','10');
+insert into panel values('D','10');
+
+
+insert into Subjects values('A','2001');
+insert into Subjects values('A','2002');
+insert into Subjects values('A','2003');
+insert into Subjects values('A','2004');
+insert into Subjects values('A','2005');
+INSERT INTO Subjects (panel_name, course_id) VALUES ('B', '2001');
+INSERT INTO Subjects (panel_name, course_id) VALUES ('B', '2002');
+INSERT INTO Subjects (panel_name, course_id) VALUES ('B', '2003');
+INSERT INTO Subjects (panel_name, course_id) VALUES ('B', '2004');
+INSERT INTO Subjects (panel_name, course_id) VALUES ('B', '2005');
+INSERT INTO Subjects (panel_name, course_id)
+VALUES ('C', '2001'),('C', '2002'),('C', '2003'),('C', '2004'),('C', '2005');
+INSERT INTO Subjects (panel_name, course_id)
+VALUES ('D', '2001'),('D', '2002'),('D', '2003'),('D', '2004');
+
+
+
+
+
+
 
 
